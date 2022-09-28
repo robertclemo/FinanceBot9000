bc872da5a369c1a89056d16d8222ee64a62c51ed
robertclemo <72224419+robertclemo@users.noreply.github.com>
2022-09-28 15:47:18 -0400

    Create Tracker.java
    Added Tracker class

---
+++ Tracker.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+
+public class Tracker {
+  public static void main(String[] args) {
+    Scanner input = new Scanner(System.in);
+    System.out.println("Enter your buying price per share: ");
+    double buyingPrice = input.nextDouble();
+  }
+}
