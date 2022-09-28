7664d6fbd9e82cb745cec9d76bf0e4b95a096db0
robertclemo <72224419+robertclemo@users.noreply.github.com>
2022-09-28 16:11:04 -0400

    Finished Tracker.java

--- Tracker.java
+++ Tracker.java
@@ -9,26 +9,33 @@ public class Tracker {
     double closingPrice;
     double[] weekly = new double[7];
     
-    input.close();
-    
     int i = 0;
-while (i < weekly.length) {
+    while (i < weekly.length) { // While loop begins to get closing price each day
 	System.out.println("Enter the closing price for the day " + day);
 	closingPrice = input.nextDouble();
 	double earnings = closingPrice - buyingPrice;
 	weekly[i] = earnings;
 
+	if (earnings > 0) {
+  	System.out.printf("After day " + day + " you have gained $%.2f" + " per share since yesterday.",earnings); // System.out.printf helps format the value of the earnings variable and just print the value to 2 precision points, i.e. 234.5
+  	System.out.println(" ");
+       } else if (earnings < 0) {
+  	System.out.printf("After day " + day + " you have lost $%.2f" + " per share since yesterday.",(-earnings)); // Used a minus sign before earnings variable since its a loss.
+  	System.out.println(" ");
+       } else {
+  	System.out.println("After day " + day + ", you have no earnings per share");
+	}
 	day = day + 1;
 	i = i + 1;
-   } // Not sure if this bracket needs to be here or not
-if (earnings > 0) {
-  System.out.printf("After day " + day + " you have gained $%.2f" + " per share since yesterday.",earnings); // System.out.printf helps format the value of the earnings variable and just print the value to 2 precision points, i.e. 234.5
-  System.out.println(" ");
-} else if (earnings < 0) {
-  System.out.printf("After day " + day + " you have lost $%.2f" + " per share since yesterday.",(-earnings)); // Used a minus sign before earnings variable since its a loss.
-  System.out.println(" ");
-} else {
-  System.out.println("After day " + day + ", you have no earnings per share");
-}
+    } // While loop ends to get closing price each day
+	System.out.println("Your weekly earnings:");
+	double totalWeeklyEarnings = 0;
+	for (int j = 0; j < weekly.length; j++) {
+		System.out.printf("Your earning on day " + j + " is $%.2f", weekly[j]);
+		System.out.println(" ");
+		totalWeeklyEarnings = totalWeeklyEarnings + weekly[j];
+	}
+	  System.out.printf("Total earnings: " + "$%.2f" + " per share", totalWeeklyEarnings);
+	  input.close();
   }
 }
